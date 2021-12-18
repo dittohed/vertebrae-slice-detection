7d27f2743ad0898f116cf12174de4c4c5664a68d
dittohed <wojtek.konieczkowicz@gmail.com>
2021-12-19 00:06:16 +0100

    Merged dev into master

---
+++ main_train.py
@@ -0,0 +1,33 @@
+from tensorflow.keras import callbacks
+import src.config as config
+import src.generator as generator
+import src.utils as utils
+import src.preprocessing as preprocessing
+import src.models as models
+import src.callbacks as callbacks
+
+if __name__ == '__main__':
+    x, y, spacings, names = utils.load_data(config.USE_FRONT)
+    x, y, slices, heights = preprocessing.normalize_data(x, y, spacings)
+
+    # optional
+    # utils.save_imgs_dist(x, 'front' if experiment[0] else 'sagittal')
+
+    x_train, y_train, \
+        x_val, y_val, spacings_val, slices_val, heights_val = utils.split_data(x, y, spacings, slices, heights)
+
+    gen_train = generator.DataGenerator(x_train, y_train, 
+                            input_shape=config.INPUT_SHAPE, batch_size=config.BATCH_SIZE, rgb=config.RGB)
+
+    # or
+    # x_val, y_val = utils.prepare_for_inference(x_val, y_val) 
+    # gen_val = generator.InferenceDataGenerator(x_val, y_val)
+    gen_val = generator.DataGenerator(x_val, y_val, validation=True,
+                            input_shape=config.INPUT_SHAPE, batch_size=config.BATCH_SIZE, rgb=config.RGB)
+
+    model = models.get_model(config.MODEL_NAME)
+    history = model.fit(gen_train, validation_data=gen_val,
+                    epochs=config.NUM_EPOCHS, 
+                    callbacks=callbacks.get_callbacks('crops_nosigma_thresh0'))
+    utils.plot_learning(history, 'crops_nosigma_thresh0')
+    # TODO: określając nazwy dostaję odpowiedni checkpoint, logi i wykresy
